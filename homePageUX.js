d91ace71d407a3fee2fd8e0665319c0ed639da86
AnneThomas87 <137282043+AnneThomas87@users.noreply.github.com>
2023-12-02 16:42:58 +0100

    Add files via upload

--- homePageUX.js
+++ homePageUX.js
@@ -18,10 +18,24 @@ function onMapClick(e) {
 
 map.on('click', onMapClick);
 
+
+// --------------fin de la map 
+
 let tabs = ['Spectacle', 'Concert', 'Vernissage', 'Exposition'];
 const recupSelect = document.querySelector('.selectAll');
 console.log(recupSelect);
 const select = document.createElement("select");
+
+select.id="selection";
+
+let content = "";
+
+
+
+
+
+
+
 
 for (let i = 0; i < tabs.length; i++) {
     let element = tabs[i];
@@ -32,6 +46,9 @@ for (let i = 0; i < tabs.length; i++) {
     select.appendChild(option);
     select.classList.add('select');
 }
+
+// ----------------fin de selectAll
+
 recupSelect.appendChild(select);
 
 const verni= document.querySelectorAll('.vernissage')
@@ -71,3 +88,170 @@ recupSelect.addEventListener("input", function(event) {
         }
     });
 });
+// ---------------fin de levent dispo en bas de la map qui reagis a mes options 
+
+const evenementsParCategorie = [
+      {
+        nom: "Concert Jazz",
+        photo: "./Asset/expo1.jpg",
+        description: "Une soirée de jazz en live avec des artistes talentueux.",
+        localisation: "Salle de concert XYZ",
+        style: "Jazz",
+        prix: 20.00,
+        categorie: "Concerts"
+      },
+      {
+        nom: "Concert Pop-Rock",
+        photo: "./Asset/expo1.jpg",
+        description: "Une soirée électrique avec des groupes de pop-rock populaires.",
+        localisation: "Stade de la Musique",
+        style: "Pop-Rock",
+        prix: 25.00,
+        categorie: "Concerts"
+      },
+      {
+        nom: "Concert Acoustique Intime",
+        photo: "./Asset/expo1.jpg",
+        description: "Une expérience musicale intime avec des performances acoustiques.",
+        localisation: "Café Acoustique",
+        style: "Acoustique",
+        prix: 12.00,
+        categorie: "Concerts"
+      },
+      {
+        nom: "Concert Acoustique Intime",
+        photo: "./Asset/expo1.jpg",
+        description: "Une expérience musicale intime avec des performances acoustiques.",
+        localisation: "Café Acoustique",
+        style: "Acoustique",
+        prix: 12.00,
+        categorie: "Concerts"
+      },
+ 
+      {
+        nom: "Exposition d'Art Moderne",
+        photo: "./Asset/expo2.jpg",
+        description: "Découvrez des œuvres d'art moderne par des artistes contemporains.",
+        localisation: "Galerie d'art ABC",
+        style: "Art Moderne",
+        prix: 10.00,
+        categorie: "Expositions"
+      },
+      {
+        nom: "Exposition Photo Nature",
+        photo: "./Asset/expo2.jpg",
+        description: "Une exposition captivante mettant en valeur la beauté de la nature.",
+        localisation: "Centre d'exposition Nature",
+        style: "Photographie",
+        prix: 12.00,
+        categorie: "Expositions"
+      },
+      {
+        nom: "Exposition Photo Nature",
+        photo: "./Asset/expo2.jpg",
+        description: "Une exposition captivante mettant en valeur la beauté de la nature.",
+        localisation: "Centre d'exposition Nature",
+        style: "Photographie",
+        prix: 12.00,
+        categorie: "Expositions"
+      },
+      {
+        nom: "Exposition Photo Urbaine",
+        photo: "./Asset/expo2.jpg",
+        description: "Explorez la vie urbaine à travers l'objectif de photographes talentueux.",
+        localisation: "Centre d'exposition Urbaine",
+        style: "Photographie",
+        prix: 10.00,
+        categorie: "Expositions"
+      },
+  
+      {
+        nom: "Spectacle de Magie",
+        photo: "./Asset/expo3.jpg",
+        description: "Un spectacle magique plein d'illusions et de mystère.",
+        localisation: "Théâtre Magique",
+        style: "Magie",
+        prix: 18.00,
+        categorie: "Spectacles"
+      },
+      {
+        nom: "Spectacle de Théâtre Dramatique",
+        photo: "./Asset/expo3.jpg",
+        description: "Une performance théâtrale émouvante qui explore des thèmes dramatiques.",
+        localisation: "Théâtre Dramatique",
+        style: "Théâtre",
+        prix: 15.00,
+        categorie: "Spectacles"
+      },
+  
+      {
+        nom: "Vernissage d'Art Contemporain",
+        photo: "./Asset/expo4.jpg",
+        description: "Rencontrez les artistes lors du vernissage d'une exposition d'art contemporain.",
+        localisation: "Espace d'Art Contemporain",
+        style: "Art Contemporain",
+        prix: 8.00,
+        categorie:  "Vernissages"
+      },
+      {
+        nom: "Vernissage de graffiti",
+        photo: "./Asset/expo4.jpg",
+        description: "Rencontrez les artistes lors du vernissage d'une exposition d'art graffiti.",
+        localisation: "Espace d'Art Graffiti",
+        style: "Graffiti",
+        prix: 8.00,
+        categorie:  "Vernissages"
+      },
+      {
+        nom: "Vernissage des enfants du monde",
+        photo: "./Asset/expo4.jpg",
+        description: "Rencontrez les artistes lors du vernissage d'une exposition réaliser par les enfants.",
+        localisation: "Espace d'Art des enfants",
+        style: "Enfant",
+        prix: 8.00,
+        categorie:  "Vernissages"
+      },
+      {
+        nom: "Vernissage des enfants du monde",
+        photo: "./Asset/expo4.jpg",
+        description: "Rencontrez les artistes lors du vernissage d'une exposition réaliser par les enfants.",
+        localisation: "Espace d'Art des enfants",
+        style: "Enfant",
+        prix: 8.00,
+        categorie:  "Vernissages"
+      }
+    ]
+  
+
+  const main = document.querySelector("main");
+  const categories = ["Concerts","Vernissages","Expositions","Spectacles"]
+
+  categories.forEach(category => {
+    content += `<h2 class="${category}" id="${category}">${category}</h2><section class="${category}">`;
+    
+    //let mesEvents=[];
+    const evenements = evenementsParCategorie.forEach(function(eventS){
+      if(eventS.categorie===category){
+        content += `
+        <article>
+        <figure>
+            <img class="event-photo" src="${eventS.photo}" alt="">
+            <div class="texte">
+            <figcaption class="event-nom">${eventS.nom}</figcaption>
+            <figcaption class="event-description">${eventS.description}</figcaption>
+            <figcaption class="event-localisation">${eventS.localisation}</figcaption>
+            <figcaption class="event-style">${eventS.style}</figcaption>
+            <figcaption> ${eventS.prix} Euros</figcaption>
+        </figure>
+          </div>
+        </article>`;
+  }
+  
+    })
+    content += "</section>";
+
+  });
+
+main.innerHTML = content;
+main.classList.add('presentation');
+
