556948db4b4fe16543662a4bc5e55de33a054c8b
AnneThomas87 <137282043+AnneThomas87@users.noreply.github.com>
2023-12-06 09:27:41 +0100

    Add files via upload

--- homePageUX.js
+++ homePageUX.js
@@ -1,6 +1,28 @@
+//----------------
+// const radios = {
+//     date: ['Actuellement', 'Aujourd\'hui', 'Demain'],
+//     prix: ['Libre', 'Payant'],
+//   };
+     
+//   const parametre = document.querySelector('.Parametre');
+  
+//   Object.keys(radios).forEach((key) => {
+//     radios[key].forEach((value) => {
+//       const input = document.createElement('input');
+//       input.dataset.id = key;
+//       input.type = 'radio';
+//       input.value = value;
+  
+//       const label = document.createElement('label');
+//       label.textContent = value;
+  
+//       parametre.appendChild(input);
+//       parametre.appendChild(label);
+//     });
+//   });
+  
 
-
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([43.2965, 5.3698], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
@@ -10,32 +32,47 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 var popup = L.popup();
 
 function onMapClick(e) {
-    popup
-        .setLatLng(e.latlng)
-        .setContent("You clicked the map at " + e.latlng.toString())
-        .openOn(map);
+    // Utilisation de l'API Nominatim pour obtenir des détails sur les coordonnées
+    fetch(`https://nominatim.openstreetmap.org/reverse?format=json&lat=${e.latlng.lat}&lon=${e.latlng.lng}&zoom=18&addressdetails=1`)
+        .then(response => response.json())
+        .then(data => {
+            // Création du contenu du popup avec des détails sur le lieu
+            const content = `
+                <strong>${data.display_name}</strong><br>
+                Type: ${data.type}<br>
+                Nom: ${data.address?.shop || data.address?.amenity || 'N/A'}<br>
+                Adresse: ${data.address?.road || data.address?.footway || 'N/A'}<br>
+                Ville: ${data.address?.city || 'N/A'}<br>
+            `;
+            
+            popup
+                .setLatLng(e.latlng)
+                .setContent(content)
+                .openOn(map);
+        })
+        .catch(error => {
+            console.error('Erreur lors de la récupération des données:', error);
+        });
 }
 
 map.on('click', onMapClick);
 
 
+
 // --------------fin de la map 
 
+// --------------------------------
+
+
+// ----------------debut de selectAll
 let tabs = ['Spectacle', 'Concert', 'Vernissage', 'Exposition'];
 const recupSelect = document.querySelector('.selectAll');
-console.log(recupSelect);
+// console.log(recupSelect);
 const select = document.createElement("select");
 
 select.id="selection";
 
 let content = "";
-
-
-
-
-
-
-
 
 for (let i = 0; i < tabs.length; i++) {
     let element = tabs[i];
@@ -46,8 +83,6 @@ for (let i = 0; i < tabs.length; i++) {
     select.appendChild(option);
     select.classList.add('select');
 }
-
-// ----------------fin de selectAll
 
 recupSelect.appendChild(select);
 
@@ -65,18 +100,6 @@ function filterByType(type) {
         return filtered
 }
 
-
-console.log(filterByType("concert"));
-
-// recupSelect.addEventListener("input", function(event) {
-// console.log(event.target.value.toLowerCase())
-// const type = event.target.value.toLowerCase()
-// const events = filterByType(type)
-// console.log(events)
-// })
-// ----------------------------
-
-
 recupSelect.addEventListener("input", function(event) {
     const type = event.target.value.toLowerCase();
     const events = filterByType(type);
@@ -88,7 +111,7 @@ recupSelect.addEventListener("input", function(event) {
         }
     });
 });
-// ---------------fin de levent dispo en bas de la map qui reagis a mes options 
+// ---------------fin de l'event dispo en bas de la map qui reagis a mes options 
 
 const evenementsParCategorie = [
       {
@@ -98,7 +121,9 @@ const evenementsParCategorie = [
         localisation: "Salle de concert XYZ",
         style: "Jazz",
         prix: 20.00,
-        categorie: "Concerts"
+        categorie: "Concerts",
+        like: "./Asset/coeurEvent.png",
+        dates: "Aujourd'hui"
       },
       {
         nom: "Concert Pop-Rock",
@@ -107,7 +132,9 @@ const evenementsParCategorie = [
         localisation: "Stade de la Musique",
         style: "Pop-Rock",
         prix: 25.00,
-        categorie: "Concerts"
+        categorie: "Concerts",
+        like: "./Asset/coeurEvent.png",
+        dates: "Aujourd'hui"
       },
       {
         nom: "Concert Acoustique Intime",
@@ -116,7 +143,9 @@ const evenementsParCategorie = [
         localisation: "Café Acoustique",
         style: "Acoustique",
         prix: 12.00,
-        categorie: "Concerts"
+        categorie: "Concerts",
+        like: "./Asset/coeurEvent.png",
+        dates: "Aujourd'hui"
       },
       {
         nom: "Concert Acoustique Intime",
@@ -125,7 +154,9 @@ const evenementsParCategorie = [
         localisation: "Café Acoustique",
         style: "Acoustique",
         prix: 12.00,
-        categorie: "Concerts"
+        categorie: "Concerts",
+        like: "./Asset/coeurEvent.png",
+        dates: "Demain"
       },
  
       {
@@ -135,7 +166,9 @@ const evenementsParCategorie = [
         localisation: "Galerie d'art ABC",
         style: "Art Moderne",
         prix: 10.00,
-        categorie: "Expositions"
+        categorie: "Expositions",
+        like: "./Asset/coeurEvent.png",
+        dates: "Demain"
       },
       {
         nom: "Exposition Photo Nature",
@@ -144,7 +177,9 @@ const evenementsParCategorie = [
         localisation: "Centre d'exposition Nature",
         style: "Photographie",
         prix: 12.00,
-        categorie: "Expositions"
+        categorie: "Expositions",
+        like: "./Asset/coeurEvent.png",
+        dates: "Du 4 Janvier au 17 Janvier"
       },
       {
         nom: "Exposition Photo Nature",
@@ -153,7 +188,10 @@ const evenementsParCategorie = [
         localisation: "Centre d'exposition Nature",
         style: "Photographie",
         prix: 12.00,
-        categorie: "Expositions"
+        categorie: "Expositions",
+        like: "./Asset/coeurEvent.png",
+        dates: "3 Mars au 11 Avril"
+        
       },
       {
         nom: "Exposition Photo Urbaine",
@@ -162,7 +200,9 @@ const evenementsParCategorie = [
         localisation: "Centre d'exposition Urbaine",
         style: "Photographie",
         prix: 10.00,
-        categorie: "Expositions"
+        categorie: "Expositions",
+        like: "./Asset/coeurEvent.png",
+        dates: "3 Mars au 11 Avril"
       },
   
       {
@@ -172,7 +212,9 @@ const evenementsParCategorie = [
         localisation: "Théâtre Magique",
         style: "Magie",
         prix: 18.00,
-        categorie: "Spectacles"
+        categorie: "Spectacles",
+        like: "./Asset/coeurEvent.png",
+        dates: "3 Mars au 11 Avril"
       },
       {
         nom: "Spectacle de Théâtre Dramatique",
@@ -181,7 +223,31 @@ const evenementsParCategorie = [
         localisation: "Théâtre Dramatique",
         style: "Théâtre",
         prix: 15.00,
-        categorie: "Spectacles"
+        categorie: "Spectacles",
+        like: "./Asset/coeurEvent.png",
+        dates: "3 Mars au 11 Avril"
+      },
+      {
+        nom: "Spectacle de Théâtre Dramatique",
+        photo: "./Asset/expo3.jpg",
+        description: "Une performance théâtrale émouvante qui explore des thèmes dramatiques.",
+        localisation: "Théâtre Dramatique",
+        style: "Théâtre",
+        prix: 15.00,
+        categorie: "Spectacles",
+        like: "./Asset/coeurEvent.png",
+        dates: "3 Mai au 13 juin"
+      },
+      {
+        nom: "Spectacle de Théâtre Dramatique",
+        photo: "./Asset/expo3.jpg",
+        description: "Une performance théâtrale émouvante qui explore des thèmes dramatiques.",
+        localisation: "Théâtre Dramatique",
+        style: "Théâtre",
+        prix: 15.00,
+        categorie: "Spectacles",
+        like: "./Asset/coeurEvent.png",
+        dates: "Aujourd'hui"
       },
   
       {
@@ -191,7 +257,9 @@ const evenementsParCategorie = [
         localisation: "Espace d'Art Contemporain",
         style: "Art Contemporain",
         prix: 8.00,
-        categorie:  "Vernissages"
+        categorie:  "Vernissages",
+        like: "./Asset/coeurEvent.png",
+        dates: "Demain"
       },
       {
         nom: "Vernissage de graffiti",
@@ -200,7 +268,9 @@ const evenementsParCategorie = [
         localisation: "Espace d'Art Graffiti",
         style: "Graffiti",
         prix: 8.00,
-        categorie:  "Vernissages"
+        categorie:  "Vernissages",
+        like: "./Asset/coeurEvent.png",
+        dates: "Actuellement jusqu'au 20 Decembre"
       },
       {
         nom: "Vernissage des enfants du monde",
@@ -209,7 +279,9 @@ const evenementsParCategorie = [
         localisation: "Espace d'Art des enfants",
         style: "Enfant",
         prix: 8.00,
-        categorie:  "Vernissages"
+        categorie:  "Vernissages",
+        like: "./Asset/coeurEvent.png",
+        dates: "Actuellement jusqu'au 20 Decembre"
       },
       {
         nom: "Vernissage des enfants du monde",
@@ -218,9 +290,16 @@ const evenementsParCategorie = [
         localisation: "Espace d'Art des enfants",
         style: "Enfant",
         prix: 8.00,
-        categorie:  "Vernissages"
+        categorie:  "Vernissages",
+        like: "./Asset/coeurEvent.png",
+        dates: "Actuellement jusqu'au 20 Decembre"
       }
     ]
+
+      
+       
+    
+    
   
 
   const main = document.querySelector("main");
@@ -242,6 +321,8 @@ const evenementsParCategorie = [
             <figcaption class="event-localisation">${eventS.localisation}</figcaption>
             <figcaption class="event-style">${eventS.style}</figcaption>
             <figcaption> ${eventS.prix} Euros</figcaption>
+            <figcaption> ${eventS.dates}</figcaption>
+            <img class="event-like" src="${eventS.like}" alt="Like">
         </figure>
           </div>
         </article>`;
@@ -254,4 +335,16 @@ const evenementsParCategorie = [
 
 main.innerHTML = content;
 main.classList.add('presentation');
+
+// ... (Votre code existant)
+
+
+
+
+// ... (Le reste de votre code)
+
+
+
+
+
 
